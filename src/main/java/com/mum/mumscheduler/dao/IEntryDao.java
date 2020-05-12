5f7d3c511b05e13141690bc596e6d71ca514ab82
deep_bohara <dpak23bohara@gmail.com>
2020-05-12 16:25:37 -0500

    added Entry CRUD

--- src/main/java/com/mum/mumscheduler/dao/IEntryDao.java
+++ src/main/java/com/mum/mumscheduler/dao/IEntryDao.java
@@ -1,4 +1,16 @@
 package com.mum.mumscheduler.dao;
 
+import com.mum.mumscheduler.models.Block;
+import com.mum.mumscheduler.models.Entry;
+import com.mum.mumscheduler.models.Student;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IEntryDao {
+    Optional<Entry> getEntryByIntake(String intake);
+    List<Entry> getAllEntry();
+    Entry add(Entry entry);
+    Entry update(Entry entry, String intake);
+    void delete(String intake);
 }
--- src/main/java/com/mum/mumscheduler/respository/EntryRepository.java
+++ src/main/java/com/mum/mumscheduler/respository/EntryRepository.java
@@ -1,4 +1,11 @@
 package com.mum.mumscheduler.respository;
 
-public interface EntryRepository {
+import com.mum.mumscheduler.models.Course;
+import com.mum.mumscheduler.models.Entry;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EntryRepository extends MongoRepository<Entry,String> {
+    //public Course findEntryByIntake(String intake);
 }
