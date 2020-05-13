370c1febcdd890b36e05f0f65aba25c835e0d798
pradipacharya7 <pradip.acharya077@gmail.com>
2020-05-13 14:19:15 -0500

    studentprofile

--- src/main/java/com/mum/mumscheduler/controllers/StudetntProfileController.java
+++ src/main/java/com/mum/mumscheduler/controllers/StudetntProfileController.java
@@ -1,6 +1,5 @@
 package com.mum.mumscheduler.controllers;
 
-import com.mum.mumscheduler.models.Student;
 import com.mum.mumscheduler.services.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,13 +12,17 @@ public class StudetntProfileController {
     @Autowired
     IStudentService studentProfileService;
 
-    @GetMapping("/studentprofile/{id}")
-    public String showProfile(@PathVariable String id, Model model)
+    @GetMapping("/studentprofile/{username}")
+    public String showProfile(@PathVariable String username, Model model)
     {
-        model.addAttribute("student",studentProfileService.getStudentByUsername("foo"));
+        model.addAttribute("student",studentProfileService.getStudentByUsername(username));
+        return "frontend/student/main";
+    }
+    @GetMapping("/studentprofile/details/{username}")
+    public String showProfiledetails(@PathVariable String username, Model model)
+    {
+        model.addAttribute("student",studentProfileService.getStudentByUsername(username));
         return "frontend/student/studentprofile";
     }
-
-
 
 }
